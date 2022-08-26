16c3766dcc0444f5a1890a415a280fc1427af576
developython14 <djmel1955@gmail.com>
2022-08-26 02:58:16 +0200

    discover react js

--- src/componants/MenuComponant.js
+++ src/componants/MenuComponant.js
@@ -2,5 +2,13 @@ import React,{Component} from "react";
 
 
 class Menu extends Component {
-    
+
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return ();
+    }
 }
+
+export default Menu;
